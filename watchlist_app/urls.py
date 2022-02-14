7dd4a8a6a7fcfb09f3bbc4e0f12be0fa5519119f
MDTalha178 <talhaazmi178@gmail.com>
2022-02-14 13:05:45 +0530

    urls update

--- watchlist_app/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import path, include
-# from watchlist_app import views
-
-# urlpatterns = [
-#     path('list/', views.movie_list, name='list'),
-#     path('<int:pk>', views.movie_details, name='movie-details'),
-# ]
