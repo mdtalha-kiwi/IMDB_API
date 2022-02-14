79d458a7cd8bea8b89251b6ec4e625689c84da2e
MDTalha178 <talhaazmi178@gmail.com>
2022-02-14 12:50:46 +0530

    Initial commit

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -95,28 +95,28 @@ class WatchDetailAV(APIView):
 #         else:
 #             return Response(serializer.errors)
 
-@api_view(['GET','PUT','DELETE'])
-def movie_details(request,pk):
-    if request.method == 'GET':
-        try:
-          movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response({'error': 'Movie not found'},status=status.HTTP_404_NOT_FOUND)
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data)
+# @api_view(['GET','PUT','DELETE'])
+# def movie_details(request,pk):
+#     if request.method == 'GET':
+#         try:
+#           movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             return Response({'error': 'Movie not found'},status=status.HTTP_404_NOT_FOUND)
+#         serializer = MovieSerializer(movie)
+#         return Response(serializer.data)
     
-    if request.method == 'PUT':
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+#     if request.method == 'PUT':
+#         movie = Movie.objects.get(pk=pk)
+#         serializer = MovieSerializer(movie,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
     
-    if request.method == 'DELETE':
-        movie = Movie.objects.get(pk=pk)
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     if request.method == 'DELETE':
+#         movie = Movie.objects.get(pk=pk)
+#         movie.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
         
         
